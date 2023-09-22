4c6c0a62e1b70e466fefefe68ca67182432f947b
= <=>
2023-09-21 20:25:59 -0500

    inicio lab6

---
+++ GTICS_LAB6_20200825/src/main/java/com/example/gtics_lab6_20200825/GticsLab620200825Application.java
@@ -0,0 +1,13 @@
+package com.example.gtics_lab6_20200825;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GticsLab620200825Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GticsLab620200825Application.class, args);
+    }
+
+}
---
+++ GTICS_LAB6_20200825/src/test/java/com/example/gtics_lab6_20200825/GticsLab620200825ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gtics_lab6_20200825;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GticsLab620200825ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
