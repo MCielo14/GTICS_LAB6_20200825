d5af24486312a86e499312cde047c84b4f613221
= <=>
2023-09-21 20:54:11 -0500

    fin de pregunta 1

---
+++ GTICS_LAB6_20200825/src/main/java/com/example/gtics_lab6_20200825/controllers/PrincipalCotrolller.java
@@ -0,0 +1,2 @@
+package com.example.gtics_lab6_20200825.controllers;public class PrincipalCotrolller {
+}
---
+++ GTICS_LAB6_20200825/src/main/java/com/example/gtics_lab6_20200825/entity/Technician.java
@@ -0,0 +1,7 @@
+package com.example.gtics_lab6_20200825;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class Technician {
+}
